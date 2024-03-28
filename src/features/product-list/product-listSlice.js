cbb605b9d610b1010a61a8f200be94b4a4ec4e20
Sarada Prasanna Dash <dashsharada@gmail.com>
2024-03-28 18:31:03 +0530

    added pagination feature

--- src/features/product-list/product-listSlice.js
+++ src/features/product-list/product-listSlice.js
@@ -4,21 +4,20 @@ import { fetchAllProducts, fetchProductsByFilters } from "./product-listApi";
 const initialState = {
   products: [],
   status: "idle",
+  totalItems:0
 };
 
 export const fetchAllProductsAsync = createAsyncThunk(
   "products/fetchAllProducts",
   async () => {
     const response = await fetchAllProducts();
-    // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
 export const fetchProductsByFiltersAsync = createAsyncThunk(
   'product/fetchProductsByFilters',
-  async ({filter,sort}) => {
-    const response = await fetchProductsByFilters(filter,sort);
-    // The value we return becomes the `fulfilled` action payload
+  async ({filter,sort,pagination}) => {
+    const response = await fetchProductsByFilters(filter,sort,pagination);
     return response.data;
   }
 );
@@ -40,12 +39,13 @@ export const productSlice = createSlice({
         state.status = 'loading';
       })
       .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
-        state.status = 'idle';
-        state.products = action.payload;
+        state.products = action.payload.products;
+        state.totalItems = action.payload.totalItems;
       });
   },
 });
 
-export const selectAllProducts = (state) => state.product.products; // Corrected selector
+export const selectAllProducts = (state) => state.product.products; 
+export const selectTotalItems = (state) => state.product.totalItems;
 
 export default productSlice.reducer;
